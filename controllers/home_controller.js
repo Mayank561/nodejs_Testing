c12100d3cd373be08c6c920118228d32b77be2e9
mayank gupta <mayankgupta.edu@gmail.com>
2023-07-01 17:32:15 +0530

    first nodejs server

---
+++ controllers/home_controller.js
@@ -0,0 +1,9 @@
+module.exports.home = function(req, res){
+    
+    return res.render('home', {
+        title: "Home"
+        // Script: Script
+    });
+}
+
+// module.exports.actionName = function(req, res){}
